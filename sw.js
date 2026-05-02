ebf6af7ae44917afc81de2d1f8810c50bb420a77
Joso1100 <josejosejose190@outlook.es>
2026-05-02 16:31:35 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'biblioteca-v2';
+const CACHE_NAME = 'biblioteca-v3'; // Pongo v3 ya directamente
 const assets = [
   './',
   './index.html',
@@ -10,9 +10,26 @@ const assets = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting(); // Fuerza a que se active de inmediato
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(assets);
+    })
+  );
+});
+
+// Este es el evento que te faltaba para borrar la caché vieja
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Borrando caché antigua:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
